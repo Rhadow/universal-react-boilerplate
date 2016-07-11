c678bf58bbe8cdba4e153a5af3b31d5c8b8cbd13
Howard Chang <howard3433@gmail.com>
2016-07-11 18:00:50 +0800

    Modify server view render method

--- src/server.js
+++ src/server.js
@@ -7,12 +7,30 @@ import appRoutes from './shared/routes';
 import configureStore from './shared/configureStore';
 
 const app = express();
+const createView = (html, state) => {
+    return `
+        <!DOCTYPE html>
+        <html>
+            <head>
+                <meta charset="utf-8">
+                <title>Universal Redux Demo</title>
+            </head>
+            <body>
+                <div id="react-view">${html}</div>
+                <script type="application/javascript">
+                    window.__INITIAL_STATE__ = ${JSON.stringify(state)};
+                </script>
+                <script type="application/javascript" src="/bundle.js"></script>
+            </body>
+        </html>`;
+}
 
 app.use((req, res) => {
     let isNotFoundPage = false,
         store,
         initialComponentHtml,
-        initialState;
+        initialState,
+        finalView;
 
     match({ routes: appRoutes(), location: req.url }, (error, redirectLocation, renderProps) => {
         if (error) {
@@ -30,22 +48,8 @@ app.use((req, res) => {
             );
         }
     });
-    const HTML = `
-        <!DOCTYPE html>
-        <html>
-            <head>
-                <meta charset="utf-8">
-                <title>Universal Redux Demo</title>
-            </head>
-            <body>
-                <div id="react-view">${initialComponentHtml}</div>
-                <script type="application/javascript">
-                    window.__INITIAL_STATE__ = ${JSON.stringify(initialState)};
-                </script>
-                <script type="application/javascript" src="/bundle.js"></script>
-            </body>
-        </html>`;
-    res.status(isNotFoundPage ? 404 : 200).send(HTML);
+    finalView = createView(initialComponentHtml, initialState);
+    res.status(isNotFoundPage ? 404 : 200).send(finalView);
 });
 
 export default app;
